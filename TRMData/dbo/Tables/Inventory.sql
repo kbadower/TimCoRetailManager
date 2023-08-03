a6da39050a7bc0d6ad8913ff56d3098069c5fc7f
Krzysztof Badower <kbadower@gmail.com>
2023-08-03 21:41:35 +0200

    Add foreign keys to tables

--- TRMData/dbo/Tables/Inventory.sql
+++ TRMData/dbo/Tables/Inventory.sql
@@ -5,4 +5,5 @@
     [Quantity] INT NOT NULL DEFAULT 1,
     [PurchasePrice] MONEY NOT NULL , 
     [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id)
 )
