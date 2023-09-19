e7c1a5071ce375cb57b2a89fbd2f4a3eaa09d68d
Krzysztof Badower <kbadower@gmail.com>
2023-09-19 22:17:52 +0200

    Add Authentication to Blazor Portal

---
+++ Portal/Authentication/AuthenticationService.cs
@@ -0,0 +1,62 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Portal.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace Portal.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthenticationService(HttpClient httpClient,
+                                     AuthenticationStateProvider authenticationStateProvider,
+                                     ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _authenticationStateProvider = authenticationStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<AuthenticatedUserModel> LogIn(AuthenticationUserModel userForAuthentication)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", userForAuthentication.Email),
+                new KeyValuePair<string, string>("password", userForAuthentication.Password)
+            });
+
+            var authResult = await _httpClient.PostAsync("https://localhost:5001/Token", data);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode == false)
+            {
+                return null;
+            }
+
+            var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            await _localStorage.SetItemAsync("authToken", result.Access_Token);
+
+            ((AuthStateProvider)_authenticationStateProvider).NotifyUserAuthentication(result.Access_Token);
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
+
+            return result;
+        }
+
+        public async Task LogOut(AuthenticatedUserModel userForLogout)
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((AuthStateProvider)_authenticationStateProvider).NotifyUserLogout();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
---
+++ Portal/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using Portal.Models;
+
+namespace Portal.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticatedUserModel> LogIn(AuthenticationUserModel userForAuthentication);
+        Task LogOut(AuthenticatedUserModel userForLogout);
+    }
+}
